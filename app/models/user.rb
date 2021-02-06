37b758fbe98288a071e949b44c8028f054f19b8b
Bakshee <bakshee.jw@gmail.com>
2021-02-06 01:13:55 +0100

    Add validations and authentication methods to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,39 @@
 class User < ApplicationRecord
+  attr_reader :password
+
+  before_validation :ensure_session_token
+  validates :username, presence: true, uniqueness: true
+  validates :password_digest, presence: true
+  validates :session_token, presence: true, uniqueness: true
+  validates :password, length: { minimum: 6, allow_nil: true }
+
+  def self.find_by_credentials(username, password)
+    user = User.find_by(username: username)
+    return user if user && user.is_password?(password)
+    nil
+  end
+
+  def self.generate_session_token
+    SecureRandom::urlsafe_base64
+  end
+
+  def ensure_session_token
+    self.session_token ||= User.generate_session_token
+  end
+
+  def reset_session_token
+    self.session_token = User.generate_session_token
+    self.save
+    self.session_token
+  end
+
+  def password=(password)
+    @password = password
+    self.password_digest = BCrypt::Password.create(password)
+  end
+
+  def is_password?(password)
+    BCrypt::Password.new(self.password_digest).is_password?(password)
+  end
 
 end
