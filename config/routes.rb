24c48ba27dff84aed8511eeb22c4f6f2045c3989
Bakshee <bakshee.jw@gmail.com>
2021-02-19 19:49:51 +0100

    Set root to cats#index

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: 'cats#index'
+
   resources :cats
 
   resources :cat_rental_requests, only: [:new, :create] do
