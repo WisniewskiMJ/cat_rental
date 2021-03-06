f8ff44d2dfb5e173082726ea74557a8ad5f387b2
Bakshee <bakshee.jw@gmail.com>
2021-03-06 14:44:39 +0100

    Add request processing capybara tests

--- spec/factories/cat_rental_requests.rb
+++ spec/factories/cat_rental_requests.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :cat_rental_request do
     cat_id { FactoryBot.build(:cat).id }
     user_id { FactoryBot.build(:user).id }
-    start_date { Date.today }
+    start_date { Date.tomorrow }
     end_date { Date.today.next_week }
   end
 end
--- spec/features/user_features_spec.rb
+++ spec/features/user_features_spec.rb
@@ -2,6 +2,8 @@ require 'rails_helper'
 
 feature 'user features', type: :feature do
 
+  let(:user) { FactoryBot.create(:user) }
+
   feature 'visiting main page' do
     scenario 'visits index page' do
       visit cats_url
@@ -26,7 +28,6 @@ feature 'user features', type: :feature do
   end
 
   feature 'signing user in' do
-      let(:user) { FactoryBot.create(:user) }
     scenario 'gets login form' do
       visit cats_url
       click_on 'Log in'
@@ -43,7 +44,6 @@ feature 'user features', type: :feature do
   end
 
   feature 'editing user' do
-    let(:user) { FactoryBot.create(:user) }
     scenario 'gets edit user form' do
       login_user(user.username, 'password')
       click_on 'Edit account'
@@ -62,11 +62,33 @@ feature 'user features', type: :feature do
   end
 
   feature 'deleting user' do
-    let(:user) { FactoryBot.create(:user) }
     scenario 'deletes user' do
       login_user(user.username, 'password')
       click_on 'Delete account'
       expect(User.find_by(username: user.username)).to be_nil
+    end
+  end
+
+  feature 'processing requests' do
+    scenario 'approves request' do
+      login_user(user.username, 'password')
+      create_cat('Valid_cat', '01/01/2020', 'grey', 'F','Description of a cat')
+      rent_cat('Valid_cat', Date.tomorrow, Date.today.next_week)
+      rent_cat('Valid_cat', Date.tomorrow, Date.today.next_week)
+      cat = Cat.find_by(name: 'Valid_cat')
+      visit cat_url(cat.id)
+      first(:link, 'Accept').click
+      expect(page).to have_content('approved')
+    end
+    scenario 'denies request' do
+      login_user(user.username, 'password')
+      create_cat('Valid_cat', '01/01/2020', 'grey', 'F','Description of a cat')
+      rent_cat('Valid_cat', Date.tomorrow, Date.today.next_week)
+      rent_cat('Valid_cat', Date.tomorrow, Date.today.next_week)
+      cat = Cat.find_by(name: 'Valid_cat')
+      visit cat_url(cat.id)
+      first(:link, 'Reject').click
+      expect(page).to have_content('denied')
     end
   end
 end
